d27c3f7b9514c2618722c82b51d5331343887579
Nándor Dudás <nandor.dudas@gmail.com>
2021-11-30 20:31:31 +0000

    chore: add composer setup

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use TicTacToe\App;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new App();
---
+++ src/App.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TicTacToe;
+
+class App
+{
+    public function __construct()
+    {
+        dump('Tic Tac Toe');
+    }
+}
