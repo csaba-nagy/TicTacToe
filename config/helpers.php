81a4841537a0c5e841f73b52a780b7e87ef6dd08
Nándor Dudás <nandor.dudas@gmail.com>
2021-12-12 13:23:16 +0000

    fix: add missing function checks

--- config/helpers.php
+++ config/helpers.php
@@ -2,35 +2,41 @@
 
 declare(strict_types=1);
 
-/**
- * Check weather the given array is associative or not.
- *
- * @param array $data
- * @return bool
- */
-function isDataAssociative(array $data): bool
-{
-    return $data !== array_values($data);
+if (!function_exists('isDataAssociative')) {
+    /**
+     * Check weather the given array is associative or not.
+     *
+     * @param array $data
+     * @return bool
+     */
+    function isDataAssociative(array $data): bool
+    {
+        return $data !== array_values($data);
+    }
 }
 
-/**
- * Get flatten array from the given array.
- *
- * @param array $data
- * @return array
- */
-function flatData(array $data): array
-{
-    return array_merge(...$data);
+if (!function_exists('flatData')) {
+    /**
+     * Get flatten array from the given array.
+     *
+     * @param array $data
+     * @return array
+     */
+    function flatData(array $data): array
+    {
+        return array_merge(...$data);
+    }
 }
 
-/**
- * Transpose array from the given array, clockwise.
- *
- * @param array $data
- * @return array
- */
-function transposeData(array $data): array
-{
-    return array_map(null, ...array_reverse($data, true));
+if (!function_exists('transposeData')) {
+    /**
+     * Transpose array from the given array, clockwise.
+     *
+     * @param array $data
+     * @return array
+     */
+    function transposeData(array $data): array
+    {
+        return array_map(null, ...array_reverse($data, true));
+    }
 }
